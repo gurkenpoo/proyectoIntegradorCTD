5f4454d4649ea7c26b9a8f22b9f2a0cc5a9246a0
gurkenpoo <bastianpoo@gmail.com>
2024-05-25 03:43:08 -0400

    Add dinamic route (nextjs) [id]

---
+++ src/app/details/[id]/page.tsx
@@ -0,0 +1,231 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { MdLocalShipping, MdCall } from 'react-icons/md';
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
+  List,
+  ListItem,
+  SkeletonText,
+  Center,
+  Link,
+  Skeleton,
+} from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import Header from '@/components/Header';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  imageUrls: string[];
+  originalPrice: number;
+  discountPrice: number;
+  category: string;
+}
+
+ function Page({ params }: { params: { id: number } }) {
+  const { id } = params;
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedProducts = localStorage.getItem('products');
+      if (storedProducts) {
+        try {
+          const parsedProducts: Product[] = JSON.parse(storedProducts);
+          const foundProduct = parsedProducts.find(p => p.id === Number(id));
+          setProduct(foundProduct || null);
+        } catch (error) {
+          console.error('Error parsing products:', error);
+        }
+      }
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000); // Retraso de 1 segundo
+    }
+  }, [id]);
+
+ if (loading) {
+  return (
+    <Container maxW={'7xl'}>
+      <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={{ base: 8, md: 10 }} py={{ base: 18, md: 24 }}>
+        <Flex>
+          <Box
+            rounded={'md'}
+            bg={useColorModeValue('white', 'gray.800')}
+            boxShadow={'2xl'}
+            p={6}
+            textAlign={'center'}
+            w={'100%'}
+            h={{ base: '100%', sm: '400px', lg: '500px' }}>
+            {/* Ajustar los estilos del Skeleton */}
+            <Skeleton size="10" height={['230px', '230px', '430px']} width={['282px', '282px', '482px']} mx="auto" mb="4" />
+          </Box>
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={'header'}>
+            <SkeletonText noOfLines={1} spacing="4" skeletonHeight="1" w={{ base: '100%', sm: '600px' }} mx="auto" />
+            <SkeletonText noOfLines={1} spacing="4" skeletonHeight="1" w={{ base: '100%', sm: '600px' }} mx="auto" />
+            <SkeletonText noOfLines={1} spacing="4" skeletonHeight="1" w={{ base: '100%', sm: '600px' }} mx="auto" />
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={'column'}
+            divider={
+              <StackDivider borderColor={useColorModeValue('gray.200', 'gray.600')} />
+            }>
+            <Box>
+              <SkeletonText noOfLines={4} spacing="4" skeletonHeight="2" w={{ base: '100%', sm: '600px' }} mx="auto" textAlign="justify" />
+            </Box>
+            <Box>
+              <SkeletonText noOfLines={3} spacing="4" skeletonHeight="1" w={{ base: '100%', sm: '600px' }} mx="auto" />
+            </Box>
+          </Stack>
+
+          <Box mx="auto" mt={8} w={{ base: '100%', sm: '400px', lg: '500px' }}>
+            <SkeletonText noOfLines={1} spacing="4" skeletonHeight="1" w={'full'} />
+          </Box>
+
+          <Stack direction='row' spacing={4} justifyContent={'center'}>
+            <SkeletonText noOfLines={1} spacing="4" skeletonHeight="1" w={'full'} />
+            <SkeletonText noOfLines={1} spacing="4" skeletonHeight="1" w={'full'} />
+          </Stack>
+        </Stack>
+      </SimpleGrid>
+    </Container>
+  );
+}
+
+  if (!product) {
+    return (
+      <Center py={12}>
+        <Text>Producto no encontrado</Text>
+      </Center>
+    );
+  }
+
+  return (
+    <><Header /><Container maxW={'7xl'}>
+          <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={{ base: 8, md: 10 }} py={{ base: 18, md: 24 }}>
+             <Stack spacing={4}>
+              {product.imageUrls.map((imageUrl, index) => (
+                <Image
+                  key={index}
+                  rounded={'md'}
+                  alt={'product image'}
+                  src={imageUrl}
+                  fit={'cover'}
+                  align={'center'}
+                  w={'100%'}
+                  h={{ base: '100%', sm: '400px', lg: '500px' }}
+                />
+              ))}
+            </Stack>
+              <Stack spacing={{ base: 6, md: 10 }}>
+                  <Box as={'header'}>
+                      <Heading
+                          lineHeight={1.1}
+                          fontWeight={600}
+                          fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+                          {product.name}
+                      </Heading>
+                      <Text color={useColorModeValue('gray.900', 'gray.400')} fontWeight={300} fontSize={'2xl'}>
+                          ${product.originalPrice}
+                      </Text>
+                      {product.discountPrice > 0 && (
+                          <Text textDecoration={'line-through'} color={'gray.600'} fontWeight={300} fontSize={'2xl'}>
+                              ${product.discountPrice}
+                          </Text>
+                      )}
+                  </Box>
+
+                  <Stack
+                      spacing={{ base: 4, sm: 6 }}
+                      direction={'column'}
+                      divider={<StackDivider borderColor={useColorModeValue('gray.200', 'gray.600')} />}>
+                      <VStack spacing={{ base: 4, sm: 1 }}>
+                          <Text
+                              maxW={{ base: '100%', sm: '600px' }}
+                              color={useColorModeValue('gray.500', 'gray.400')}
+                              fontSize={{ base: 'xl', sm: 'xl', md: 'xl', lg: '2xl' }}
+                              fontWeight={'300'}
+                              textAlign={{ base: 'left', sm: 'justify' }}>
+                              {product.description}
+                          </Text>
+                      </VStack>
+                      <Box>
+                          <Text fontSize={{ base: '16px', lg: '18px' }} color={useColorModeValue('yellow.500', 'yellow.300')} fontWeight={'500'} textTransform={'uppercase'} mb={'4'}>
+                              Product Details
+                          </Text>
+                          <List spacing={2}>
+                              <ListItem>
+                                  <Text as={'span'} fontWeight={'bold'}>
+                                      Category:
+                                  </Text>{' '}
+                                  {product.category}
+                              </ListItem>
+                              <ListItem>
+                                  <Text as={'span'} fontWeight={'bold'}>
+                                      Original Price:
+                                  </Text>{' '}
+                                  ${product.originalPrice}
+                              </ListItem>
+                              {product.discountPrice > 0 && (
+                                  <ListItem>
+                                      <Text as={'span'} fontWeight={'bold'}>
+                                          Discount Price:
+                                      </Text>{' '}
+                                      ${product.discountPrice}
+                                  </ListItem>
+                              )}
+                          </List>
+                      </Box>
+                  </Stack>
+
+                  <Button
+                      rounded={'15'}
+                      w={'full'}
+                      mt={8}
+                      size={'lg'}
+                      py={'7'}
+                      bg={useColorModeValue('gray.900', 'gray.50')}
+                      color={useColorModeValue('white', 'gray.900')}
+                      textTransform={'uppercase'}
+                      _hover={{
+                          transform: 'translateY(2px)',
+                          boxShadow: 'lg',
+                      }}>
+                      Agregar al Carrito
+                  </Button>
+
+                  <Stack direction="row" alignItems="center" justifyContent={'center'}>
+                      <MdLocalShipping />
+                      <Text> El correo de confirmacion llegara en unos minutos</Text>
+                  </Stack>
+                  <Stack direction='row' spacing={4} justifyContent={'center'}>
+                      <Button as={Link} href='/#ListaProductos' leftIcon={<ArrowBackIcon />} colorScheme='pink' variant='solid'>
+                          Volver
+                      </Button>
+                  </Stack>
+              </Stack>
+          </SimpleGrid>
+      </Container></>
+  );
+}
+
+export default Page;
