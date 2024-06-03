36f1ba4b20bf384512490d2a32bfc69aaec806cf
gurkenpoo <bastianpoo@gmail.com>
2024-06-03 01:50:35 -0400

    add header 'cerrar sesion' and redirect to signIn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
-'use client';
-import { Box, Button, Flex, Stack, useColorModeValue } from '@chakra-ui/react';
+'use client'
+import { Box, Button, Flex, Stack, useColorModeValue, Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, Input } from '@chakra-ui/react';
 import { Image, Link } from '@chakra-ui/next-js';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation'; // Importa useRouter
 import { Avatar } from '@chakra-ui/react';
 import { UserInt } from '@/interfaces/UserInt'; // Importa la interfaz UserInt
 
+
 const Header = () => {
   const [user, setUser] = useState<UserInt | null>(null); // Usa UserInt para tipar user
   const router = useRouter(); // Inicializa useRouter
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false); // Estado para controlar si el Drawer está abierto
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -16,7 +18,7 @@ const Header = () => {
         const token = localStorage.getItem('token');
         if (!token) {
           console.error('No token found');
-          router.push('/login/signIn'); // Redirige al usuario si no hay token
+          // router.push('/login/signIn'); // Redirige al usuario si no hay token
           return;
         }
 
@@ -40,69 +42,119 @@ const Header = () => {
     fetchUserProfile();
   }, [router]); // Asegúrate de incluir router en las dependencias
 
-  return (
-    <Box as="header" position="fixed" top="0" w="full" zIndex={1000}>
-      <Flex
-        maxWidth="100vw"
-        w="full"
-        bg={useColorModeValue('white', 'gray.800')}
-        color={useColorModeValue('gray.600', 'white')}
-        minH={'60px'}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.900')}
-        align={'center'}
-        justify={'space-between'}
-      >
-        <Flex
-          w="full"
-          maxW={'sm'}
-          flex={{ base: 1 }}
-          justify={{ base: 'center', md: 'start' }}
-        >
-          <Box>
-            <Link href={'/'}>
-              <Image src="/logoVino.png" width={160} height={140} alt="logo" />
-            </Link>
-          </Box>
-          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-            {/* <DesktopNav /> */}
-          </Flex>
-        </Flex>
+  // Función para abrir el Drawer
+  const handleOpenDrawer = () => {
+    setIsDrawerOpen(true);
+  };
 
-        <Stack
-          flex={{ base: 1, md: 0 }}
-          justify={'flex-end'}
-          direction={'row'}
-          spacing={6}
+  // Función para cerrar el Drawer
+  const handleCloseDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
+  // Función para cerrar sesión
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Elimina el token del localStorage
+    router.push("/login/signIn"); // Redirige al home
+  };
+
+  return (
+    <>
+      <Box as="header" position="fixed" top="0" w="full" zIndex={1000}>
+        <Flex
+          maxWidth="100vw"
+          w="full"
+          bg={useColorModeValue('white', 'gray.800')}
+          color={useColorModeValue('gray.600', 'white')}
+          minH={'60px'}
+          py={{ base: 2 }}
+          px={{ base: 4 }}
+          borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.900')}
+          align={'center'}
+          justify={'space-between'}
         >
-          {user ? (
-            <AvatarWithInitials user={user} /> // Renderiza AvatarWithInitials si hay un usuario
-          ) : (
-            <>
-              <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'/login/signIn'}>
-                Iniciar Sesión
+          <Flex
+            w="full"
+            maxW={'sm'}
+            flex={{ base: 1 }}
+            justify={{ base: 'center', md: 'start' }}
+          >
+            <Box>
+              <Link href={'/'}>
+                <Image src="/logoVino.png" width={160} height={140} alt="logo" />
+              </Link>
+            </Box>
+            <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+              {/* <DesktopNav /> */}
+            </Flex>
+          </Flex>
+
+          <Stack
+            flex={{ base: 1, md: 0 }}
+            justify={'flex-end'}
+            direction={'row'}
+            spacing={6}
+          >
+            {user ? (
+              // Avatar que abre el Drawer al hacer clic
+              <Box onClick={handleOpenDrawer} cursor="pointer">
+                <AvatarWithInitials user={user} />
+              </Box>
+            ) : (
+              <>
+                <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'/login/signIn'}>
+                  Iniciar Sesión
+                </Button>
+                <Button
+                  as={'a'}
+                  fontSize={'sm'}
+                  fontWeight={600}
+                  color={'white'}
+                  bg={'#b592c3'}
+                  href={'/login/signUp'}
+                  _hover={{
+                    bg: '#292864',
+                  }}
+                >
+                  Crear Sesión
+                </Button>
+              </>
+            )}
+
+            {/* Botón de Cerrar Sesión */}
+          </Stack>
+        </Flex>
+      </Box>
+
+      {/* Drawer */}
+      <Drawer
+        isOpen={isDrawerOpen}
+        placement='right'
+        onClose={handleCloseDrawer}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>User Profile</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+            <Button mt={6} onClick={handleLogout} colorScheme='pink' variant='solid'>
+                Cerrar Sesión
               </Button>
-              <Button
-                as={'a'}
-                fontSize={'sm'}
-                fontWeight={600}
-                color={'white'}
-                bg={'#b592c3'}
-                href={'/login/signUp'}
-                _hover={{
-                  bg: '#292864',
-                }}
-              >
-                Crear Sesión
-              </Button>
-            </>
-          )}
-        </Stack>
-      </Flex>
-    </Box>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={handleCloseDrawer}>
+              Cancel
+            </Button>
+                          
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
